27a334ab2fa90b6836f48fa8507c8c74dbf189a9
nouer <hadi.nouer@gmail.com>
2021-02-06 19:56:27 +0900

    stater project

---
+++ src/test/java/com/kadal/ilang/aplikasiregistrasi/AplikasiRegistrasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadal.ilang.aplikasiregistrasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiRegistrasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
